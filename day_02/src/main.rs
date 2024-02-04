017e91fdb63b5d212f5c825dfd2ef2d12cfbaec0
antongigele <anton.gigele@aon.at>
2024-02-04 01:16:20 +0100

    day_02 part1 solved

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,3 +1,49 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+// use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let _ = day_02_part1("input.txt");
+}
+
+pub fn day_02_part1(filepath: &str) -> std::io::Result<()>{
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let num_of_red_cubes = 12;
+    let num_of_green_cubes = 13;
+    let num_of_blue_cubes = 14;
+    let mut game_sum = 0;
+
+    for result_line in reader.lines() {
+        let line = result_line?;
+
+        let colon_parts = line.split(":").collect::<Vec<&str>>();
+        let id = colon_parts.get(0).unwrap().split(" ").collect::<Vec<&str>>();
+        let turns_split_by_semicolon = colon_parts.get(1).unwrap().split(";").collect::<Vec<&str>>();
+        let mut add_to_sum = true;
+
+        for i in 0..turns_split_by_semicolon.len() {
+            let results_split_by_comma = turns_split_by_semicolon.get(i).unwrap().split(",").collect::<Vec<&str>>();
+            for result in results_split_by_comma {
+                let split_result = result.trim().split(" ").collect::<Vec<&str>>();
+                let cubes_num = split_result.get(0).unwrap().parse::<i32>().unwrap();
+                
+                if split_result.get(1).unwrap() == &"red" && cubes_num > num_of_red_cubes {
+                    add_to_sum = false;
+                }
+                else if split_result.get(1).unwrap() == &"green" && cubes_num > num_of_green_cubes {
+                    add_to_sum = false;
+                }
+                else if split_result.get(1).unwrap() == &"blue" && cubes_num > num_of_blue_cubes {
+                    add_to_sum = false;
+                }
+            }
+        }
+        if add_to_sum {
+            game_sum += id.get(1).unwrap().parse::<i32>().unwrap();
+        }
+    }
+    println!("Result is {}", game_sum);
+    Ok(())
 }
