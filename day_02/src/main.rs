564bf7a0e68b9f4f4f442b2512a3ba088c980ec4
antongigele <anton.gigele@aon.at>
2024-02-04 21:36:28 +0100

    day 2 finished

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     let _ = day_02_part1("input.txt");
+    let _ = day_02_part2("input.txt");
 }
 
 pub fn day_02_part1(filepath: &str) -> std::io::Result<()>{
@@ -29,13 +30,13 @@ pub fn day_02_part1(filepath: &str) -> std::io::Result<()>{
                 let split_result = result.trim().split(" ").collect::<Vec<&str>>();
                 let cubes_num = split_result.get(0).unwrap().parse::<i32>().unwrap();
                 
-                if split_result.get(1).unwrap() == &"red" && cubes_num > num_of_red_cubes {
+                if *split_result.get(1).unwrap() == "red" && cubes_num > num_of_red_cubes {
                     add_to_sum = false;
                 }
-                else if split_result.get(1).unwrap() == &"green" && cubes_num > num_of_green_cubes {
+                else if *split_result.get(1).unwrap() == "green" && cubes_num > num_of_green_cubes {
                     add_to_sum = false;
                 }
-                else if split_result.get(1).unwrap() == &"blue" && cubes_num > num_of_blue_cubes {
+                else if *split_result.get(1).unwrap() == "blue" && cubes_num > num_of_blue_cubes {
                     add_to_sum = false;
                 }
             }
@@ -45,5 +46,44 @@ pub fn day_02_part1(filepath: &str) -> std::io::Result<()>{
         }
     }
     println!("Result is {}", game_sum);
+    Ok(())
+}
+
+pub fn day_02_part2(filepath: &str) -> std::io::Result<()>{
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let mut sum_min_products = 0;
+
+    for result_line in reader.lines() {
+        let line = result_line?;
+
+        let mut min_num_of_red_cubes = 1;
+        let mut min_num_of_green_cubes = 1;
+        let mut min_num_of_blue_cubes = 1;
+
+        let colon_parts = line.split(":").collect::<Vec<&str>>();
+        let turns_split_by_semicolon = colon_parts.get(1).unwrap().split(";").collect::<Vec<&str>>();
+
+        for i in 0..turns_split_by_semicolon.len() {
+            let results_split_by_comma = turns_split_by_semicolon.get(i).unwrap().split(",").collect::<Vec<&str>>();
+            for result in results_split_by_comma {
+                let split_result = result.trim().split(" ").collect::<Vec<&str>>();
+                let cubes_num = split_result.get(0).unwrap().parse::<i32>().unwrap();
+                
+                if *split_result.get(1).unwrap() == "red" && cubes_num > min_num_of_red_cubes {
+                    min_num_of_red_cubes = cubes_num;
+                }
+                else if *split_result.get(1).unwrap() == "green" && cubes_num > min_num_of_green_cubes {
+                    min_num_of_green_cubes = cubes_num;
+                }
+                else if *split_result.get(1).unwrap() == "blue" && cubes_num > min_num_of_blue_cubes {
+                    min_num_of_blue_cubes = cubes_num;
+                }
+            }
+        }
+        sum_min_products += min_num_of_red_cubes * min_num_of_green_cubes * min_num_of_blue_cubes;
+    }
+    println!("Result is {}", sum_min_products);
     Ok(())
 }
